cf1be21059d1bf6bf8b40dce7448ce2748dbbe6d
keilyhernandez <keily.hernandez@catolica.edu.sv>
2021-03-04 23:17:52 -0600

    subiendo los ejercicios

---
+++ ejercicio1/ejercicio1/Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+    Sub Main()
+        Dim Elementos(10) As Integer
+        Dim suma As Integer = 0
+
+        For i = 0 To 9 Step 1
+            Console.WriteLine("Inserte un número " & i + 1 & ": ")
+            Elementos(i) = Console.ReadLine()
+            suma += Elementos(i)
+
+        Next
+
+        For j = 0 To 9 Step 1
+            Console.WriteLine("Valor " & j + 1 & ":" & Elementos(j))
+        Next
+
+        Console.WriteLine("La suma es: " & suma)
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ ejercicio2/ejercicio2/Module1.vb
@@ -0,0 +1,41 @@
+﻿Shared Sub Main(args As String())
+    Dim I As Integer = 0
+    Dim K As Integer = 0
+    Dim CAN As Integer = 0
+    Dim AUX As Integer = 0
+    Dim linea As String
+    Console.Write("CUANTOS ELEMENTOS MÁX=12:")
+    linea = Console.ReadLine()
+    CAN = Integer.Parse(linea)
+    Dim VEC(0 To CAN + 1 - 1) As Integer
+    ' INGRESO
+    For I = 1 To CAN
+        Console.Write("POSICIÓN {0} ==>", I)
+        linea = Console.ReadLine()
+        VEC(I) = Integer.Parse(linea)
+
+    Next
+    ' PROCESO
+    For I = 1 To CAN - 1
+        For K = I + 1 To CAN
+            If (VEC(I) < VEC(K)) Then
+
+                AUX = VEC(K)
+                VEC(K) = VEC(I)
+                VEC(I) = AUX
+
+            End If
+
+        Next
+    Next
+    Console.WriteLine()
+    ' SALIDA
+    Console.WriteLine("ARREGLO ORDENADO DESCENDENTE")
+    For I = 1 To CAN
+        Console.WriteLine(VEC(I))
+
+    Next
+    Console.Write("Pulse una Tecla:")
+    Console.ReadLine()
+
+End Sub
